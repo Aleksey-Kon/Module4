68eb3f5a2701946e1f62c01f3a662c4bd5b82590
Aleksey-Kon <windowsviser@gmail.com>
2024-04-10 20:48:20 +0300

    Initial commit

---
+++ Assets/MyProject/Scripts/player.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+public class Player : MonoBehaviour
+{
+    [SerializeField] private CharacterController _characterController;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float _speed;
+
+    private Vector3 _input;
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift)) _speed = 6;
+        else _speed = 3;
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        _input = new Vector3(horizontal, 0, vertical);
+
+        Vector3 movementVector = _camera.transform.TransformDirection(_input);
+        //_camera.transform.RotateAround()
+        movementVector.y = 0;
+        movementVector.Normalize();
+        transform.forward = movementVector;
+
+        _characterController.Move(movementVector * (_speed * Time.deltaTime));
+        _animator.SetFloat("Speed", _characterController.velocity.magnitude);
+    }
+}
